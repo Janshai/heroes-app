f52ec324e7e8f39482d023fc10c87391924e9d00
Janshai <jack.r.s.adams@gmail.com>
2021-06-04 21:29:15 +0100

    base project

---
+++ app/src/main/java/com/heroes/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.heroes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.heroes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
